896d4a33dec58ade5e8c040e174594d90a453cc4
hoshin01 <unihoshin23d@gmail.com>
2024-07-12 04:19:48 +0900

    ロックエミッター作成

--- Source/Game/Object/Effect/Rock/RockEffect.h
+++ Source/Game/Object/Effect/Rock/RockEffect.h
@@ -23,17 +23,21 @@ public:
 	// 描画
 	void Render(bool isShadow = false);
 
-	// ロック追加
-	void Emit(DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 vel, DirectX::XMFLOAT2 scaleMinMax);
 
 	struct RockEmitter
 	{
-		DirectX::XMFLOAT3 position;
-		float timer = 0.0f;
-		float destoryTime = 10.0f;
-		int generateCount = 10;
+		DirectX::XMFLOAT3 position;	// 生成座標
+		DirectX::XMFLOAT3 angle;	// 生成角度(この角度から回転しません)
+		DirectX::XMFLOAT3 scale;	// 生成スケール
+		float gravity;				// 重力
+		float lifeTime = 10.0f;		// 寿命[s](この時間がたつと消えます)
 	};
 
+	// ロック追加
+	void Emit(DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 vel, DirectX::XMFLOAT2 scaleMinMax);
+
+	// ロック追加
+	void Emit(RockEmitter rockEmitter);
 
 	std::unique_ptr<RockMainMesh> rockMesh1;
 private:

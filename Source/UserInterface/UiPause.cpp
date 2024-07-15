457cce7a04b0aa83309b86b32130d268433ad575
hoshin01 <unihoshin23d@gmail.com>
2024-07-15 17:17:20 +0900

    ポーズui 仮完成

--- Source/UserInterface/UiPause.cpp
+++ Source/UserInterface/UiPause.cpp
@@ -1,17 +1,21 @@
 #include "UiPause.h"
 #include "../../Library/Timer.h"
 #include "../../Library/Input/InputManager.h"
+#include "../Scene/SceneManager.h"
 
 
 void UiPause::Initialize()
 {
 	state = UiPauseState::Hidden;
 
+	isPause = false;
+
 	SetAllOffRender();
 	imgBlack->SetColorA(0.0f);
 	imgPauseText->SetColorA(0.0f);
 	imgResume->SetColorA(0.0f);
 	imgGoTitle->SetColorA(0.0f);
+	imgResumeGoTitleSelect->SetColorA(0.0f);
 }
 
 bool UiPause::Update()
@@ -27,6 +31,9 @@ bool UiPause::Update()
 				imgPauseText->SetIsRender(true);
 				imgResume->SetIsRender(true);
 				imgGoTitle->SetIsRender(true);
+				imgResumeGoTitleSelect->SetIsRender(true);
+
+				checkResumeGoTitleState = static_cast<int>(CheckResumeGoTitle::Resume);
 
 				isPause = true;
 				state   = UiPauseState::Showing;
@@ -41,6 +48,7 @@ bool UiPause::Update()
 			imgPauseText->SetColorA(Easing::GetNowParam(Easing::OutQuad<float>, pauseTimer, imgPauseTextAlpha));
 			imgResume->SetColorA(Easing::GetNowParam(Easing::OutQuad<float>, pauseTimer, imgPauseTextAlpha));
 			imgGoTitle->SetColorA(Easing::GetNowParam(Easing::OutQuad<float>, pauseTimer, imgPauseTextAlpha));
+			imgResumeGoTitleSelect->SetColorA(Easing::GetNowParam(Easing::OutQuad<float>, pauseTimer, imgPauseTextAlpha));
 
 			// 表示中にポーズ解除されたら非表示遷移状態に遷移
 			if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::Back))
@@ -61,11 +69,52 @@ bool UiPause::Update()
 		break;
 	case UiPauseState::Visible:
 		{
-			// ポーズフラグがfalseになったら遷移
-			if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::Back))
+			if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::A))
 			{
-				isPause = false;
-				state   = UiPauseState::Hide;
+				checkResumeGoTitleState--;
+			}
+			if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::D))
+			{
+				checkResumeGoTitleState++;
+			}
+			checkResumeGoTitleState = (checkResumeGoTitleState + static_cast<int>(CheckResumeGoTitle::END)) % static_cast<int>(CheckResumeGoTitle::END);
+
+			switch (static_cast<CheckResumeGoTitle>(checkResumeGoTitleState))
+			{
+			case CheckResumeGoTitle::Resume:
+				{
+					imgResumeGoTitleSelect->SetPos({ 400, 550 });
+
+					if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::Enter))
+					{
+						isPause = false;
+						state = UiPauseState::Hide;
+					}
+				}
+				break;
+			case CheckResumeGoTitle::GoTitle:
+				{
+					imgResumeGoTitleSelect->SetPos({ 700, 550 });
+
+					if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::Enter))
+					{
+						imgPauseText->SetIsRender(false);
+						imgResume->SetIsRender(false);
+						imgGoTitle->SetIsRender(false);
+						imgResumeGoTitleSelect->SetIsRender(false);
+
+						imgReallyBox->SetIsRender(true);
+						imgYesText->SetIsRender(true);
+						imgNoText->SetIsRender(true);
+						imgYesNoSelect->SetIsRender(true);
+
+						checkYesNoState = static_cast<int>(CheckYesNo::No);
+
+						state = UiPauseState::Check;
+					}
+
+				}
+				break;
 			}
 		}
 		break;
@@ -77,6 +126,7 @@ bool UiPause::Update()
 			imgPauseText->SetColorA(Easing::GetNowParam(Easing::OutQuad<float>, pauseTimer, imgPauseTextAlpha));
 			imgResume->SetColorA(Easing::GetNowParam(Easing::OutQuad<float>, pauseTimer, imgPauseTextAlpha));
 			imgGoTitle->SetColorA(Easing::GetNowParam(Easing::OutQuad<float>, pauseTimer, imgPauseTextAlpha));
+			imgResumeGoTitleSelect->SetColorA(Easing::GetNowParam(Easing::OutQuad<float>, pauseTimer, imgPauseTextAlpha));
 
 			// 表示中にポーズ解除されたら非表示遷移状態に遷移
 			if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::Back))
@@ -93,11 +143,63 @@ bool UiPause::Update()
 				imgPauseText->SetIsRender(false);
 				imgResume->SetIsRender(false);
 				imgGoTitle->SetIsRender(false);
+				imgResumeGoTitleSelect->SetIsRender(false);
 
 				pauseTimer = 0.0f;
 				state      = UiPauseState::Hidden;
 				break;
 			}
+		}
+		break;
+	case UiPauseState::Check:
+		{
+			if(InputManager::Instance().GetKeyPressed(DirectX::Keyboard::A))
+			{
+				checkYesNoState--;
+			}
+			if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::D))
+			{
+				checkYesNoState++;
+			}
+			checkYesNoState = (checkYesNoState + static_cast<int>(CheckYesNo::END)) % static_cast<int>(CheckYesNo::END);
+
+			switch (static_cast<CheckYesNo>(checkYesNoState))
+			{
+				case CheckYesNo::Yes:
+				{
+					imgYesNoSelect->SetPos({ 440, 400 });
+
+					if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::Enter))
+					{
+						SceneManager::Instance().ChangeScene(new SceneTitle);
+						break;
+					}
+				}
+				break;
+				case CheckYesNo::No:
+				{
+					imgYesNoSelect->SetPos({ 840, 400 });
+
+					if (InputManager::Instance().GetKeyPressed(DirectX::Keyboard::Enter))
+					{
+						imgPauseText->SetIsRender(true);
+						imgResume->SetIsRender(true);
+						imgGoTitle->SetIsRender(true);
+						imgResumeGoTitleSelect->SetIsRender(true);
+
+						imgReallyBox->SetIsRender(false);
+						imgYesText->SetIsRender(false);
+						imgNoText->SetIsRender(false);
+						imgYesNoSelect->SetIsRender(false);
+						state = UiPauseState::Visible;
+					}
+					
+				}
+				break;
+			}
+
+
+			
 		}
 		break;
 	}
@@ -114,6 +216,11 @@ void UiPause::Render()
 	imgPauseText->Render();
 	imgResume->Render();
 	imgGoTitle->Render();
+	imgResumeGoTitleSelect->Render();
+	imgReallyBox->Render();
+	imgYesText->Render();
+	imgNoText->Render();
+	imgYesNoSelect->Render();
 }
 
 void UiPause::SetAllOffRender()
--- Source/UserInterface/UiPause.h
+++ Source/UserInterface/UiPause.h
@@ -14,12 +14,36 @@ private:
 		imgPauseText->UpdateAnimation();
 		imgPauseText->SetPos({ 640, 360 });
 		sprites.emplace_back(imgPauseText.get());
-		imgResume = std::make_unique<Sprite>("Data/Texture/UserInterface/Pause/resume.png");
+		imgResume = std::make_unique<Sprite>("Data/Texture/UserInterface/Pause/resume.sprite");
+		imgResume->UpdateAnimation();
 		imgResume->SetPos({ 400, 550 });
 		sprites.emplace_back(imgResume.get());
-		imgGoTitle = std::make_unique<Sprite>("Data/Texture/UserInterface/Pause/goTitle.png");
+		imgGoTitle = std::make_unique<Sprite>("Data/Texture/UserInterface/Pause/goTitle.sprite");
+		imgGoTitle->UpdateAnimation();
 		imgGoTitle->SetPos({ 700, 550 });
 		sprites.emplace_back(imgGoTitle.get());
+		imgResumeGoTitleSelect = std::make_unique<Sprite>("Data/Texture/UserInterface/Pause/resumeGoTitleSelect.sprite");
+		imgResumeGoTitleSelect->UpdateAnimation();
+		imgResumeGoTitleSelect->SetPos({ 400, 550 });
+		sprites.emplace_back(imgResumeGoTitleSelect.get());
+
+
+		imgReallyBox = std::make_unique<Sprite>("Data/Texture/UserInterface/Pause/reallyBox.sprite");
+		imgReallyBox->UpdateAnimation();
+		imgReallyBox->SetPos({ 640, 300 });
+		sprites.emplace_back(imgReallyBox.get());
+		imgYesText = std::make_unique<Sprite>("Data/Texture/UserInterface/Pause/yesText.sprite");
+		imgYesText->UpdateAnimation();
+		imgYesText->SetPos({ 440, 400 });
+		sprites.emplace_back(imgYesText.get());
+		imgNoText = std::make_unique<Sprite>("Data/Texture/UserInterface/Pause/noText.sprite");
+		imgNoText->UpdateAnimation();
+		imgNoText->SetPos({ 840, 400 });
+		sprites.emplace_back(imgNoText.get());
+		imgYesNoSelect = std::make_unique<Sprite>("Data/Texture/UserInterface/Pause/yesNoSelect.sprite");
+		imgYesNoSelect->UpdateAnimation();
+		imgYesNoSelect->SetPos({ 840, 400 });
+		sprites.emplace_back(imgYesNoSelect.get());
 	};
 
 	~UiPause()
@@ -58,12 +82,13 @@ private:
 		Showing,
 		Visible,
 		Hide,
+		Check,
 	};
 	UiPauseState state;
 
 	std::vector<Sprite*> sprites;
 
-	float pauseTimer     = 0.0f; // ポーズ状態管理用タイマー
+	float pauseTimer = 0.0f; // ポーズ状態管理用タイマー
 	float pauseTotalTime = 0.2;  // ポーズUIの遷移完了までの時間
 
 	std::unique_ptr<Sprite> imgBlack;
@@ -82,4 +107,28 @@ private:
 
 	std::unique_ptr<Sprite> imgResume;
 	std::unique_ptr<Sprite> imgGoTitle;
+	std::unique_ptr<Sprite> imgResumeGoTitleSelect;
+
+	enum class CheckResumeGoTitle
+	{
+		Resume,
+		GoTitle,
+		END,
+	};
+	int checkResumeGoTitleState;
+
+
+	// check
+	enum class CheckYesNo
+	{
+		Yes,
+		No,
+		END,
+	};
+	int checkYesNoState;
+
+	std::unique_ptr<Sprite> imgReallyBox;
+	std::unique_ptr<Sprite> imgYesText;
+	std::unique_ptr<Sprite> imgNoText;
+	std::unique_ptr<Sprite> imgYesNoSelect;
 };
